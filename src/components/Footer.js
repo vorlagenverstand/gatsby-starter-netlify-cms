3278614762eacfdcf8880b0ffb68394128351ff2
vorlagenverstand <mercadojavier@outlook.com>
2020-05-13 18:56:02 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="Minde"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
@@ -31,17 +31,17 @@ const Footer = class extends React.Component {
                     </li>
                     <li>
                       <Link className="navbar-item" to="/about">
-                        About
+                        Acerca de
                       </Link>
                     </li>
                     <li>
                       <Link className="navbar-item" to="/products">
-                        Products
+                        Servicios
                       </Link>
                     </li>
                     <li>
                       <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
+                        Subir Documento
                       </Link>
                     </li>
                     <li>
@@ -62,12 +62,12 @@ const Footer = class extends React.Component {
                   <ul className="menu-list">
                     <li>
                       <Link className="navbar-item" to="/blog">
-                        Latest Stories
+                        Casos de Exito
                       </Link>
                     </li>
                     <li>
                       <Link className="navbar-item" to="/contact">
-                        Contact
+                        Contacto
                       </Link>
                     </li>
                   </ul>
