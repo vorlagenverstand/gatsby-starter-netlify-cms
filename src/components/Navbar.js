cb6f73cb0784dd001fcf7b3e24cfea1a41c3ffda
vorlagenverstand <mercadojavier@outlook.com>
2020-05-13 18:54:07 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Minde" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -61,19 +61,19 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                Acerca de
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Servicios
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Contacto
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Subir Documento
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
