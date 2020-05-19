1aa691ae5f709225cd0b5bfd4b735f5a092e9097
vorlagenverstand <mercadojavier@outlook.com>
2020-05-19 10:01:38 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
+import youtube from '../img/social/youtube.svg'
 import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
