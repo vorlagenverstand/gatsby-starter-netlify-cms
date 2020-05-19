e5aba280594a64cefd4340d47c815a219bc64903
vorlagenverstand <mercadojavier@outlook.com>
2020-05-19 09:24:35 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,9 +72,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contacto
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Subir Documento
-              </Link>
             </div>
             <div className="navbar-end has-text-centered">
               <a
@@ -83,9 +80,6 @@ const Navbar = class extends React.Component {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
               </a>
             </div>
           </div>
